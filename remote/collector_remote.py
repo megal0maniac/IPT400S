1abab52a75bb3f237f7e5c626a0d3dd88d413145
Michael Rodger <github@atinyhedgehog.za.net>
2016-08-06 23:22:44 +0200

    Fixed timing, added currentdb file, other small improvements

--- remote/collector_remote.py
+++ remote/collector_remote.py
@@ -1,25 +1,53 @@
 import sqlite3
 import os
 import sys
+from datetime import date
 import time
 import serial
 import ast
 
 wind_dir = {0 : 'N', 1 : 'NNW', 2 : 'NW', 3 : 'WNW', 4 : 'W', 5 : 'WSW', 6 : 'SW', 7 : 'SSW', 8 : 'S', 9 : 'SSE', 10 : 'SE', 11 : 'ESE', 12 : 'E', 13 : 'ENE', 14 : 'NE', 15 : 'NNE', 16 : 'N'}
-resolution = 2
+resolution = 60
+
 #TODO: This should become a variable later, both baud and device
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
 
+def writeToDB(weatherdata):
+    today = date.fromtimestamp(weatherdata['time'])
+    currentdb = 'db/{}-{:02d}-{:02d}.db'.format(today.year, today.month, today.day)
+    newdb = not os.path.exists(currentdb)
+    conn = sqlite3.connect(currentdb)
+    if newdb:
+        #create schema
+        conn.execute("CREATE TABLE weather(time INT PRIMARY KEY NOT NULL, windspeed FLOAT, winddirection INT, temperature FLOAT, humidity FLOAT, rain INT)")
+        #if the file is new, update the currentdb file. Hard-coded to write only the filename minus directory
+        f = open('db/currentdb', 'w')
+        f.write(currentdb.split('/')[1])
+        f.close()
+
+    conn.execute("""INSERT INTO weather values({}, {}, {}, {}, {}, {})""".format(weatherdata['time'], weatherdata['ws'], weatherdata['wd'], weatherdata['t'], weatherdata['h'], weatherdata['rain']))
+    conn.commit()
+    conn.close()
+
+def getFilename(timestamp):
+    today = date.fromtimestamp(timestamp)
+    return 'db/{}-{:02d}-{:02d}.db'.format(today.year, today.month, today.day)
+
 #Read data from serial every minute
-#This relies on reading serial data taking less than 59 seconds
+#This relies on reading serial data taking less than 59 seconds, should be fine?
+
+
+flag = 0
+lasttime = int(time.time()) - resolution
 while True:
-    if int(time.time())%resolution != 0:
-        time.sleep(1)
-    else:
-        #serial code here
+    if ((int(time.time())%resolution == 0) and (lasttime != int(time.time()))):
         ser.flush()
         ser.write(' ') #TODO: Single byte generates response, this should be more specific
         data = ast.literal_eval(ser.readline())
-        print '{} Temperature: {}C, Humidity: {}%, Wind speed: {}km/h, Wind direction: {}, Rain (since last reading): {}mm'.format(int(time.time()) - int(time.time())%resolution, data['t'], data['h'], data['ws'], wind_dir[data['wd']], data['rain']*2.46)
-        time.sleep(1)
-
+        lasttime = data['time'] = int(time.time())
+        print 'Condition: {} Lasttime: {} DBTime: {} Real time: {}'.format(int(time.time())%resolution, lasttime, data['time'], time.time())
+        #print '{} Temperature: {}C, Humidity: {}%, Wind speed: {}km/h, Wind direction: {}, Rain (since last reading): {}mm'.format(int(time.time()), data['t'], data['h'], data['ws'], wind_dir[data['wd']], data['rain']*2.46)
+        writeToDB(data)
+        time.sleep(0.2)
+    else:
+        time.sleep(0.2)
