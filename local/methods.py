1558ead685668f0d281e28346b5f989ace46463a
Michael Rodger <github@atinyhedgehog.za.net>
2016-08-07 23:32:45 +0200

    Added parallel sort pseudocode, removed unnecessary Python file (if this commit even works)

--- local/methods.py
+++ local/methods.py
@@ -1,5 +1,6 @@
-#Graphing methods for dealing with more data points than we want
+#Pseudocode (although valid Python) for some of the trickier tasks which I haven't gotten around to implementing
 
+#Graphing methods for dealing with more data points than we want
 start = 82
 end = 1416
 res = 100
@@ -16,3 +17,8 @@ for x in reversed(range(0,1+(end-start)/int)):
   print end - x*int
 for x in range((end-start)/int, -1, -1):
   print end - x*int
+
+#Parallel list sorting
+perm = sorted(xrange(len(foo)), key=lambda x:foo[x])
+for p in perm:
+  print "%s: %s" % (foo[p], bar[p])
