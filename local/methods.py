d901c705df2aa156802c9ec44a55bd7200150dcc
Michael Rodger <btech@packer-debian-8-amd64.digitalocean.com>
2016-08-28 13:04:17 +0200

    Added averaging code, fixed integer/int mess

--- local/methods.py
+++ local/methods.py
@@ -4,19 +4,23 @@
 start = 82
 end = 1416
 res = 100
-int = 300
+interval = 300
 
 #For getting a graph with res data points
 #Loop generates a list, use list to get average for each group and return
 for x in range(1,res+1):
   print int(start + x*((end-start)/float(res)))
 
+#Same thing as above, but for getting 'value' out of biglist
+for x in range(0,res):
+  print average([biglist[x]['value'] for x in range(int(x*((end+1.0)/res)),int((x+1)*((end+1.0)/res)))])
+
 #For getting a graph with int interval between points. Truncates oldest data
 #Same as above, otherwise
-for x in reversed(range(0,1+(end-start)/int)):
-  print end - x*int
-for x in range((end-start)/int, -1, -1):
-  print end - x*int
+for x in reversed(range(0,1+(end-start)/interval)):
+  print end - x*interval
+for x in range((end-start)/interval, -1, -1):
+  print end - x*interval
 
 #Parallel list sorting
 #Thanks Nick! http://stackoverflow.com/a/2223828
